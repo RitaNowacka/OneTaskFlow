6e82002a30c5844f6cff3b5a3d9f8c384a83fb37
Rita Nowacka <62250745+RitaNowacka@users.noreply.github.com>
2026-08-20 12:47:28 +0200

    Rename cache version to OneTaskFlow-v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "task-flow-v1";
+const CACHE = "OneTaskFlow-v1";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
